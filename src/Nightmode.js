42a17b3e214bfa5f62fff34a344f164fd8ca7abc
dillonmd <dillonm.davis@yahoo.com>
2019-03-22 11:46:12 -0500

    Style Button

--- src/Nightmode.js
+++ src/Nightmode.js
@@ -1,35 +1,25 @@
-import React, {Component} from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import './Nightmode.css';
-
-library.add(faMoon);
+import React, { Component } from "react";
+import "./Nightmode.css";
 
 // Night Mode Toggle
 
 function nightToggle() {
-    if (document.body.className === "") {
+  if (document.body.className === "") {
     document.body.className = "dark";
-    }
-        else {
-            document.body.className = "";
-        }
+  } else {
+    document.body.className = "";
+  }
 }
 
 //Night Mode Button
 class NightMode extends Component {
-  render(){ 
-      return (
-          <button 
-          id="nightmode"
-          className="night-btn"
-          onClick={nightToggle}
-          >
-              <FontAwesomeIcon icon="moon" />
-          </button>
-      );
-      }
-    }
+  render() {
+    return (
+      <div className="container">
+        <button onClick={nightToggle}>Night Mode</button>
+      </div>
+    );
+  }
+}
 
-  export default NightMode;
+export default NightMode;
